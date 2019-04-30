62fb7f1f4015052f6ddd0aa304f36463ad4f4274
Kannan321 <ananthu1998@gmail.com>
2019-04-30 11:48:48 +0530

    with comments

--- car_Automation.py
+++ car_Automation.py
@@ -12,7 +12,7 @@ from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 from imgaug import augmenters as iaa
 import cv2
-import pandas as pdZ
+import pandas as pd
 # path last  part cut
 import ntpath
 import random
